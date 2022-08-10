3a09f7b59a4f80969a45c9e14b01212900df8373
unknown <pasanpahasara7788@gmail.com>
2022-08-10 18:37:58 +0530

    Introducing User Profile View Form

--- src/lk/ijse/multiChatApplication/controller/UserChatRoomController.java
+++ src/lk/ijse/multiChatApplication/controller/UserChatRoomController.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
@@ -21,6 +22,12 @@ public class UserChatRoomController {
     public Circle showProPic;
     public Label clientName;
     public JFXButton profileBtn;
+    public Pane profile;
+    public Label fullName;
+    public Label phoneNo;
+    public Label gender;
+    public ImageView proImage;
+    public TextField fileChoosePath;
 
     public void sendMessageByKey(KeyEvent keyEvent) {
     }
@@ -35,5 +42,11 @@ public class UserChatRoomController {
     }
 
     public void handleProfileBtn(ActionEvent actionEvent) {
+    }
+
+    public void saveImage(ActionEvent actionEvent) {
+    }
+
+    public void chooseImageButton(ActionEvent actionEvent) {
     }
 }
