1771cfc03c11052ee56496d10ff8c4179d12639f
unknown <pasanpahasara7788@gmail.com>
2022-08-10 18:09:27 +0530

    Introducing UserChatRoom View Form

---
+++ src/lk/ijse/multiChatApplication/controller/UserChatRoomController.java
@@ -0,0 +1,39 @@
+package lk.ijse.multiChatApplication.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+
+/**
+ * @author : Pasan Pahasara
+ * @since : 0.1.0
+ **/
+public class UserChatRoomController {
+    public Pane chat;
+    public TextArea messageRoom;
+    public TextField msgField;
+    public Circle showProPic;
+    public Label clientName;
+    public JFXButton profileBtn;
+
+    public void sendMessageByKey(KeyEvent keyEvent) {
+    }
+
+    public void selectEmoji(MouseEvent mouseEvent) {
+    }
+
+    public void selectImage(MouseEvent mouseEvent) {
+    }
+
+    public void handleSendEvent(MouseEvent mouseEvent) {
+    }
+
+    public void handleProfileBtn(ActionEvent actionEvent) {
+    }
+}
