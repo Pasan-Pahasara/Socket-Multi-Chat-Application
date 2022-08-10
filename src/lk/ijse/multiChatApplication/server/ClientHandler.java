560f613b7b81a2b8a09c75ecf485cac6f782b27a
unknown <pasanpahasara7788@gmail.com>
2022-08-11 01:09:53 +0530

    Introducing Client Handler

---
+++ src/lk/ijse/multiChatApplication/server/ClientHandler.java
@@ -0,0 +1,11 @@
+package lk.ijse.multiChatApplication.server;
+
+/**
+ * @author : Pasan Pahasara
+ * @since : 0.1.0
+ **/
+public class ClientHandler extends Thread{
+    public ClientHandler(){
+
+    }
+}
