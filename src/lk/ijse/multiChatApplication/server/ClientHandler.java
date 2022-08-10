84c6721957e4a34e9f7612cde22512f30b9b496f
unknown <pasanpahasara7788@gmail.com>
2022-08-11 01:19:53 +0530

    Fully Refactor Client Handler Method Into The ClientHandler

--- src/lk/ijse/multiChatApplication/server/ClientHandler.java
+++ src/lk/ijse/multiChatApplication/server/ClientHandler.java
@@ -1,11 +1,30 @@
 package lk.ijse.multiChatApplication.server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
 /**
  * @author : Pasan Pahasara
  * @since : 0.1.0
  **/
-public class ClientHandler extends Thread{
-    public ClientHandler(){
+public class ClientHandler extends Thread {
+    private Socket socket;
+    private ArrayList<ClientHandler> clients;
+    private BufferedReader reader;
+    private PrintWriter writer;
 
+    public ClientHandler(Socket socket, ArrayList<ClientHandler> clients) {
+        try {
+            this.socket = socket;
+            this.clients = clients;
+            this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.writer = new PrintWriter(socket.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
