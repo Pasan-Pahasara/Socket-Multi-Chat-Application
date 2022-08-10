f28f6c8dd212addd56f2e24fde431f551093a994
unknown <pasanpahasara7788@gmail.com>
2022-08-10 19:00:51 +0530

    Fully Refactored User Registration

--- src/lk/ijse/multiChatApplication/controller/LoginFormController.java
+++ src/lk/ijse/multiChatApplication/controller/LoginFormController.java
@@ -7,12 +7,16 @@ import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import lk.ijse.multiChatApplication.model.User;
+
+import java.util.ArrayList;
 
 /**
  * @author : Pasan Pahasara
  * @since : 0.1.0
  **/
 public class LoginFormController {
+    public static ArrayList<User> users = new ArrayList<>();
     public Pane pnSignIn;
     public PasswordField txtPassword;
     public TextField txtUsername;
@@ -49,7 +53,52 @@ public class LoginFormController {
         txtPassword.setText("");
     }
 
+    /**
+     * User Registration
+     */
     public void registration(ActionEvent actionEvent) {
+        if (!txtRegName.getText().equalsIgnoreCase("")
+                && !txtRegPassword.getText().equalsIgnoreCase("")
+                && !txtRegPhoneNo.getText().equalsIgnoreCase("")
+                && (male.isSelected() || female.isSelected())) {
+            if (checkUser(txtRegName.getText())) {
+                User newUser = new User();
+                newUser.name = txtRegName.getText();
+                newUser.password = txtRegPassword.getText();
+                newUser.phoneNo = txtRegPhoneNo.getText();
+                if (male.isSelected()) {
+                    newUser.gender = "Male";
+                } else {
+                    newUser.gender = "Female";
+                }
+                users.add(newUser);
+                goBack.setOpacity(1);
+                success.setOpacity(1);
+                makeDefault();
+            }
+        }
+    }
+
+    /**
+     * Default Details
+     */
+    private void makeDefault() {
+        txtRegName.setText("");
+        txtRegPassword.setText("");
+        txtRegPhoneNo.setText("");
+        male.setSelected(true);
+    }
+
+    /**
+     * Check User
+     */
+    private boolean checkUser(String username) {
+        for (User user : users) {
+            if (user.name.equalsIgnoreCase(username)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public void login(ActionEvent actionEvent) {
