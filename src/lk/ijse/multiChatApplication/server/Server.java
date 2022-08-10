61c9be9bfaef091aa8359457c909899b7c8e6afd
unknown <pasanpahasara7788@gmail.com>
2022-08-11 01:34:18 +0530

    Introducing Client Handler Thread Connected To The Server

--- src/lk/ijse/multiChatApplication/server/Server.java
+++ src/lk/ijse/multiChatApplication/server/Server.java
@@ -3,22 +3,29 @@ package lk.ijse.multiChatApplication.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 /**
  * @author : Pasan Pahasara
  * @since : 0.1.0
  **/
 public class Server {
+    private static final ArrayList<ClientHandler> clients = new ArrayList<>();
+
     public static void main(String[] args) {
         final int PORT = 8000;
         try {
             ServerSocket serverSocket = new ServerSocket(PORT);
-            while (true){
+            while (true) {
                 System.out.println("Waiting for clients...");
                 Socket localSocket = serverSocket.accept();
                 System.out.println("Port " + localSocket.getPort());
                 System.out.println("IP " + localSocket.getInetAddress());
                 System.out.println("Connected");
+
+                ClientHandler clientThread = new ClientHandler(localSocket, clients);
+                clients.add(clientThread);
+                clientThread.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
