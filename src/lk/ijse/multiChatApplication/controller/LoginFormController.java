d04ab539549d57f9b68e4b6e8f7a5e28fcd375b8
unknown <pasanpahasara7788@gmail.com>
2022-08-10 17:43:41 +0530

    Introducing SIGN UP Form

--- src/lk/ijse/multiChatApplication/controller/LoginFormController.java
+++ src/lk/ijse/multiChatApplication/controller/LoginFormController.java
@@ -2,9 +2,9 @@ package lk.ijse.multiChatApplication.controller;
 
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 
 /**
@@ -17,10 +17,28 @@ public class LoginFormController {
     public TextField txtUsername;
     public Label loginNotifier;
     public JFXButton btnSignUp;
+    public Pane pnSignUp;
+    public ImageView btnBack;
+    public TextField txtRegName;
+    public TextField txtRegPassword;
+    public TextField txtRegPhoneNo;
+    public ToggleGroup Gender;
+    public RadioButton male;
+    public RadioButton female;
+    public Label controlRegLabel;
+    public Label success;
+    public Label goBack;
+    public JFXButton btnLogin;
 
     public void handleButtonAction(ActionEvent actionEvent) {
     }
 
     public void login(ActionEvent actionEvent) {
+    }
+
+    public void registration(ActionEvent actionEvent) {
+    }
+
+    public void backLogin(MouseEvent mouseEvent) {
     }
 }
